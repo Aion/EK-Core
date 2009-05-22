169493ac21b5506153d984efa09826bf299af694
Aion <aion@eternal-knights.de>
2009-05-22 16:53:39 +0200

    Add forgotten SQL-Files for bugreport and anti-exit

---
+++ sql/EK/2_mangos_mangos_string_PSYCITY.sql
@@ -0,0 +1,2 @@
+UPDATE `mangos_string` SET `content_default`='You are leaving the Psy-City. To leave it is not allowed. Please hold you on the Psy-Agreement!' WHERE (`entry`='11003');
+UPDATE `mangos_string` SET `content_loc3`='Du hast die Psy-City verlassen. Leider ist dies nicht gestattet! Du wurdest zurück zur Psy-City geportet!' WHERE (`entry`='11003');

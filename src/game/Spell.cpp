a92741819765a6d4698054568414bedad318c309
Aion <aion@eternal-knights.de>
2009-05-22 02:22:16 +0200

    Try to Fixx unkillable NPC's

--- src/game/Spell.cpp
+++ src/game/Spell.cpp
@@ -1201,6 +1201,9 @@ void Spell::DoAllEffectOnTarget(TargetInfo *target)
 
 void Spell::DoSpellHitOnUnit(Unit *unit, const uint32 effectMask)
 {
+    if (m_caster->hasUnitState(UNIT_STAT_DIED)) 
+        return;
+
     if(!unit || !effectMask)
         return;
 

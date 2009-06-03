9ad256e75e88b9efb5c143aeaa052132fcf5ac2e
Aion <aion@eternal-knights.de>
2009-06-03 15:44:47 +0200

    Creatures stops working on death state

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -9433,6 +9433,7 @@ void Unit::setDeathState(DeathState s)
         CombatStop();
         DeleteThreatList();
         ClearComboPointHolders();                           // any combo points pointed to unit lost at it death
+        StopMoving();
 
         if(IsNonMeleeSpellCasted(false))
             InterruptNonMeleeSpells(false);
